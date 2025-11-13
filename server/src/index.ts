bd8cf595257f29ae40b6574c961a2da8ec563375
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2025-11-13 11:32:18 +0700

    prettier fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import authUser from './middlewares/authUser.middlewares';
+import authUser from './middlewares/authUser.middlewares'
 import connectDB from './database/database'
 import userRouter from './routers/User.routers'
 
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/auth', authUser);
+app.use('/api/auth', authUser)
 app.use('/api/user', userRouter)
 
 // app.get("/", () => console.log("Hello, it's jwt website !"))
