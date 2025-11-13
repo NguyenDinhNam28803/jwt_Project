2ed8593e226401df06a8fb68b62b4c537e53f058
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2025-11-14 00:24:14 +0700

    Add client

--- server/src/middlewares/authUser.middlewares.ts
+++ server/src/middlewares/authUser.middlewares.ts
@@ -1,18 +1,37 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import User, { IUser } from "../models/User";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser;
+        }
+    }
+}
+
+interface JwtPayload {
+    _id: string;
+}
 
 const authUser = async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "Unauthorized - No token provided" });
     }
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-        req.user = decoded;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+        const user = await User.findById(decoded._id).select('-password');
+        
+        if (!user) {
+            return res.status(401).json({ message: "Unauthorized - User not found" });
+        }
+        
+        req.user = user;
         next();
     } catch (error) {
-        return res.status(401).json({ message: "Unauthorized" });
+        return res.status(401).json({ message: "Unauthorized - Invalid token" });
     }
 }
 
--- server/src/type.d.ts
+++ server/src/type.d.ts
@@ -1,10 +1,10 @@
-import { Request } from 'express'
-import { JwtPayload } from 'jsonwebtoken'
+// import { Request } from 'express'
+// import { JwtPayload } from 'jsonwebtoken'
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: string | JwtPayload
-    }
-  }
-}
+// declare global {
+//   namespace Express {
+//     interface Request {
+//       user?: string | JwtPayload
+//     }
+//   }
+// }
