a096168976d87161ecf598651c00ccaf5b00dd0f
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2025-11-14 01:53:41 +0700

    fix route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,9 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+      </Routes>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
