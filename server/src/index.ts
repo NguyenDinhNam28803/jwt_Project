e0387dc38533e7714d89632c1d7bdffcd574d81d
Nguyendinhnam28803 <nguyendinhnam241209@gmail.com>
2025-11-20 04:12:49 +0700

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,14 +11,7 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(
-  cors({
-    origin: 'http://localhost:5000', // Port của Vite dev server
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  })
-)
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/user', userRouter)
